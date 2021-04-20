fdec681c0471f4e9b75cbc1341b1f7cab21b1ea5
jakew90 <74878482+jakew90@users.noreply.github.com>
2021-04-20 16:22:14 -0400

    Update New.js

--- New.js
+++ New.js
@@ -40,6 +40,9 @@
 
 	//drag and drop functions:
 	//https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
+	
+	//This function could be deleted
+
 	function drop_handler(ev) {
 		console.log("Drop");
 		ev.preventDefault();
