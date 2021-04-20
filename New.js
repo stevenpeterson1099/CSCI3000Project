af0ead4eb8b27076996fbd387bc9af340aed4b50
jakew90 <74878482+jakew90@users.noreply.github.com>
2021-04-20 16:25:49 -0400

    Update New.js

--- New.js
+++ New.js
@@ -38,57 +38,6 @@
 		if( txtDecpassphrase.value.length>0 && objFile ) { btnDecrypt.disabled=false; } else { btnDecrypt.disabled=true; }
 	}
 
-	//drag and drop functions:
-	//https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/File_drag_and_drop
-	
-	//This function could be deleted
-
-	function drop_handler(ev) {
-		console.log("Drop");
-		ev.preventDefault();
-		// If dropped items aren't files, reject them
-		var dt = ev.dataTransfer;
-		if (dt.items) {
-			// Use DataTransferItemList interface to access the file(s)
-			for (var i=0; i < dt.items.length; i++) {
-				if (dt.items[i].kind == "file") {
-					var f = dt.items[i].getAsFile();
-					console.log("... file[" + i + "].name = " + f.name);
-					objFile=f;
-				}
-			}
-		} else {
-			// Use DataTransfer interface to access the file(s)
-			for (var i=0; i < dt.files.length; i++) {
-				console.log("... file[" + i + "].name = " + dt.files[i].name);
-			}
-			objFile=file[0];
-		}
-		displayfile()
-		if(mode=='encrypt') { encvalidate(); } else if(mode=='decrypt') { decvalidate(); }
-	}
-
-	function dragover_handler(ev) {
-		console.log("dragOver");
-		// Prevent default select and drag behavior
-		ev.preventDefault();
-	}
-
-	function dragend_handler(ev) {
-		console.log("dragEnd");
-		// Remove all of the drag data
-		var dt = ev.dataTransfer;
-		if (dt.items) {
-			// Use DataTransferItemList interface to remove the drag data
-			for (var i = 0; i < dt.items.length; i++) {
-				dt.items.remove(i);
-			}
-		} else {
-			// Use DataTransfer interface to remove the drag data
-			ev.dataTransfer.clearData();
-		}
-	}
-
 	function selectfile(Files) {
 		objFile=Files[0];
 		displayfile()
