33d1905130065f2716084c69646263e7961875bb
HeavyHeathen <62107197+HeavyHeathen@users.noreply.github.com>
2021-04-27 15:37:25 -0400

    Update main.js

--- main.js
+++ main.js
@@ -78,6 +78,27 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
 
 //Encrypt
+  const ddl = document.getElementById('ciphers')
+  const selectedValue = ddl.options[ddl.selectedIndex].value
+  if (selectedValue === 'aes') {
+    function encodeAES () {
+
+    }
+  } else if (selectedValue === 'des') {
+    function encodeDES () {
+
+    }
+  } else if (selectedValue === 'rc4') {
+	  
+    else if (selectedValue === 'blowfish') {
+    function encodeBLOWFISH () {
+
+    }
+  } else if (selectedValue === 'vigenere') {
+    function encodeVIG () {
+
+    }
+  }
 
 
 
