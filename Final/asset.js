0ee85f7742cce68f291ef8ee5c66417e18ee5390
jakew90 <74878482+jakew90@users.noreply.github.com>
2021-04-29 14:38:51 -0400

    Update asset.js

--- Final/asset.js
+++ Final/asset.js
@@ -41,11 +41,6 @@ $(function(){
 
         file = e.target.files[0];
 
-        if(file.size > 1024*1024){
-            alert('Please choose files smaller than 1mb, otherwise you may crash your browser. \nThis is a known issue. See the tutorial.');
-            return;
-        }
-
         step(3);
     });
 
